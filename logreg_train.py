882c727dec67d3af4061f51521b204af1251fdb6
0aubsq <0aubsq@gmail.com>
2020-11-21 23:09:59 +0100

    Add basic train w/ cost graph

--- logreg_train.py
+++ logreg_train.py
@@ -1,60 +1,76 @@
 import argparse
+import sys
+import math
 import numpy as np
 import pandas as pd
+from models.Model import Model
+
+def get_mean(a):
+	a = a[~pd.isnull(a)]
+	return (sum(a) / len(a))
+
+def get_var(a):
+	a = a[~pd.isnull(a)]
+	if (len(a) < 2):
+		return (0)
+	m = get_mean(a) 
+	return (sum([(ai - m)**2 for ai in a]) / (len(a) - 1))
+
+def get_std(a):
+	return (math.sqrt(get_var(a)))
+
+def normalize(d):
+	e = (d - get_mean(d)) / get_std(d)
+	return (e)
+
+def	load_file(n):
+	try:
+		d = pd.read_csv(n, sep = ',')
+	except Exception as e:
+		print ("error : {0}".format(e))
+		sys.exit()
+	return (d)
 
 def logreg_train(args):
-  df = pd.read_csv(args.dataset)
+	d = load_file(args.dataset)
+	try:
+		# Sanitize dataset
+		d = d.dropna(subset=['Herbology', 'Ancient Runes', 'Astronomy'])
+		X = np.array(d.values[:, [8, 12, 7]], dtype=float)
+		y = d.values[:, 1]
 
-  df = df.dropna(subset=['Herbology', 'Ancient Runes', 'Astronomy'])
-  X = np.array(df.values[:, [8, 12, 7]], dtype=float)
-  y = df.values[:, 1]
+		# Init model
+		if args.stochastic:
+			args.batch = 1
+		model = Model(args.iter, args.learning, args.batch > 0, args.batch, args.precision, args.visualizer)
+		
+		# Normalize features
+		X = np.array([normalize(t) for t in X.T]).T
+		new_df = pd.DataFrame(X)
 
-  if args.batch > 0:
-    pass
+		# Convert guild names to integers indexes)
+		Y = []
+		for i in y:
+			Y.append(model.feature_i[i])
+		y = np.array(Y, dtype=int)
+		y_unique = np.unique(y)
 
-  if args.stochastic:
-    pass
+		# Execute logistic regression
+		model.process_logreg(X, y)
+	except Exception as e:
+		print ("error : {0}".format(e))
 
-  if args.precision:
-    pass
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("dataset", help="select a valid dataset")
+	parser.add_argument("-i", "--iter", help="set the number of iterations (default to 1000)", default=1000)
+	parser.add_argument("-l", "--learning", help="set the learning rate (default to 0.1)", default=0.1)
+	parser.add_argument("-b", "--batch", help="set the number of batchs for batch gradient descent algorithm", default=0)
+	parser.add_argument("-s", "--stochastic", help="use the stochastic gradient descent algorithm", action="store_true")
+	parser.add_argument("-p", "--precision", help="show the precision", action="store_true")
+	parser.add_argument("-v", "--visualizer", help="show the resulting graphs", action="store_true")
+	args = parser.parse_args()
+	logreg_train(args)
 
-  if args.visualizer:
-    pass
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("dataset", help="select a valid dataset")
-    parser.add_argument(
-        "-i",
-        "--iter",
-        help="set the number of iterations (default to 1000)",
-        default=1000,
-    )
-    parser.add_argument(
-        "-b",
-        "--batch",
-        help="set the batch gradient descent algorithm",
-        default=0,
-    )
-    parser.add_argument(
-        "-s",
-        "--stochastic",
-        help="use the stochastic gradient descent algorithm",
-        action="store_true",
-    )
-    parser.add_argument(
-        "-p",
-        "--precision",
-        help="show the precision",
-        action="store_true",
-    )
-    parser.add_argument(
-      "-v",
-      "--visualizer",
-      help="show the resulting graphs",
-      action="store_true",
-    )
-
-    args = parser.parse_args()
-    logreg_train(args)
+if (__name__ == "__main__"):
+	main()
---
+++ models/Model.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class Model:
+	def __init__(self, epoch=1000, learning=0.1, batch=False, nBatch=1, verbose=False, visu=False):
+		self.epoch = int(epoch)
+		self.learning = float(learning)
+		self.batch = bool(batch) 
+		self.nBatch = int(nBatch) 
+		self.verbose = bool(verbose) 
+		self.visu = bool(visu)
+		self.feature_i = {'Ravenclaw': 0, 'Slytherin': 1, 'Gryffindor': 2, 'Hufflepuff': 3}
+		self.i_feature = {0: 'Ravenclaw', 1: 'Slytherin', 2: 'Gryffindor', 3: 'Hufflepuff'}
+		self.cost = [[],[],[],[]]
+		self.ret = pd.DataFrame(columns=['One', 'Herbology', 'Ancient Runes', 'Astronomy'])
+
+	def sigmoid(self, z):
+		return ((1 / (1 + np.exp(-z))))
+	
+	def hypothesis(self, theta, X):
+		return (self.sigmoid(np.dot(X, theta)))
+
+	def loss(self, theta, X, y):
+		h = self.hypothesis(theta, X)
+		return ((-1 / X.shape[0]) * np.sum((y * np.log(h)) + ((1 - y) * np.log(1 - h))))
+
+	def expandList(self, lst): 
+		return list(map(lambda el:[el], lst)) 
+	
+	def gradiant_descent(self, X, y):
+		X = np.insert(X, 0, 1, axis=1)
+		for h in np.unique(y):
+			if (self.verbose):
+				print("Now processing : " + self.i_feature[h])
+			y_h = np.array(self.expandList(np.where(y == h, 1, 0)))
+			self.theta = np.zeros((X.shape[1], 1))
+			for i in range(self.epoch):
+				grad = np.dot(X.T, (self.hypothesis(self.theta, X) - y_h)) / len(y_h)
+				self.theta -= (self.learning * grad)
+				self.cost[h].append(self.loss(self.theta, X, y_h))
+				if (self.verbose and self.epoch >= 10 and (i + 1) % (self.epoch // 10) == 0):
+					print("Epoch " + str(i + 1) + " / " + str(self.epoch) + " : Loss : " + str(self.loss(self.theta, X, y_h)))
+			self.ret.loc[self.i_feature[h]] = [item for sublist in self.theta for item in sublist]
+	
+	def display_cost(self):
+		for h in range(len(self.feature_i)):
+			plt.plot(self.cost[h], label=self.i_feature[h])
+		plt.legend(list(self.feature_i.keys()))
+		plt.title("Logistic Regression loss function evolution")
+		plt.show()	
+	
+	def process_logreg(self, X, y):
+		if (self.batch == False):
+			self.gradiant_descent(X, y)
+		if (self.visu == True):
+			self.display_cost()
+		self.ret.to_csv("output.csv", index_label='Hogwarts House', header=['One', 'Herbology', 'Ancient Runes', 'Astronomy'])
+		print("output.csv successfully writen !")

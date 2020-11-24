41f1b6d78054ada1219f4dba76a736fe3881de6a
Jeremie <jeremiegthr@gmail.com>
2020-11-24 23:53:42 +0100

    add option to enable GGG

--- logreg_predict.py
+++ logreg_predict.py
@@ -62,39 +62,42 @@ def greek_god_graph(houses, X, model):
 	plt.show()
 
 def logreg_predict(args):
-	d = load_file(args.dataset)
-	v = load_file(args.values)
+  d = load_file(args.dataset)
+  v = load_file(args.values)
 
-	try:
-		# Sanitize dataset
-		d = d.fillna(0)
+  try:
+    # Sanitize dataset
+    d = d.fillna(0)
 
-		# Normalize features
-		X = np.array(d.values[:, [8, 12, 7]], dtype=float)
-		X = np.array([normalize(t) for t in X.T]).T
-		X = np.insert(X, 0, 1, axis=1)
+    # Normalize features
+    X = np.array(d.values[:, [8, 12, 7]], dtype=float)
+    X = np.array([normalize(t) for t in X.T]).T
+    X = np.insert(X, 0, 1, axis=1)
 
-		theta = np.array(v.values[:, 1:].T, dtype=float)
+    theta = np.array(v.values[:, 1:].T, dtype=float)
 
-		model = Model()
-		prediction = model.hypothesis(theta, X)
+    model = Model()
+    prediction = model.hypothesis(theta, X)
 
-		# Convert integers indexes to guild names)
-		houses = np.argmax(prediction, axis=1)
-		matching_houses = list(map(lambda v: model.i_feature[v], houses))
+    # Convert integers indexes to guild names)
+    houses = np.argmax(prediction, axis=1)
+    matching_houses = list(map(lambda v: model.i_feature[v], houses))
 
-		write_prediction(matching_houses)
-		print("houses.csv successfully written !")
-		greek_god_graph(matching_houses, X, model)
+    write_prediction(matching_houses)
+    print("houses.csv successfully written !")
+    
+    if args.show:
+      greek_god_graph(matching_houses, X, model)
 
-	except Exception as e:
-		print ("error : {0}".format(e))
-		sys.exit(-1)
+  except Exception as e:
+    print ("error : {0}".format(e))
+    sys.exit(-1)
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("dataset", help="select a valid dataset")
 	parser.add_argument("values", help="select a file with trained values")
+	parser.add_argument("-s", "--show", help="show a 3D representation of the data", action="store_true")
 	args = parser.parse_args()
 	logreg_predict(args)
 
